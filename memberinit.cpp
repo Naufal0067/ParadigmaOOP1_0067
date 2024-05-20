955551eccf05ce9ae155b8ec267110ccc1469f53
Naufal0067 <naufalopang1928@gmail.com>
2024-05-20 14:35:31 +0700

    class mahasiswa, menambahkan public

---
+++ memberinit.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+class mahasiswa {
+private:
+    const int id;
+    string nama;
+    float nilai;
+
+public:
+    mahasiswa(int pId, string pNama, float pNilai) :id(pId), nama(pNama), nilai(pNilai)
+    { //Member Intialization List
+        ///definisi
+    }
+
+    ~mahasiswa() {
+        cout << "Id    = " << id << endl;
+        cout << "Nama  = " << nama << endl;
+        cout << "Nilai = " << nilai << endl;
+    }
+};
