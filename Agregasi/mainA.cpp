bd81fe8e577e307114715854b42f8130862bd891
Naufal0067 <naufalopang1928@gmail.com>
2024-05-20 15:28:00 +0700

    menambahkan var ibu cetak anak dan delete

--- Agregasi/mainA.cpp
+++ Agregasi/mainA.cpp
@@ -16,4 +16,12 @@ int main() {
     varIbu->tambahAnak(varAnak2);
     varIbu2->tambahAnak(varAnak3);
     varIbu2->tambahAnak(varAnak1);
+
+    varIbu->cetakAnak();
+    varIbu2->cetakAnak();
+
+    delete varIbu;
+    delete varIbu2;
+    delete varAnak1;
+    delete varAnak2;
 }
